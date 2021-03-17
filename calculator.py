eaa8aefcae5f6bf1b34d6e98bd661f44e58ece90
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-03-17 03:26:32 +0000

    Adding functions to get the equation results

--- calculator.py
+++ calculator.py
@@ -3,5 +3,48 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+# repeat forever:
+ 
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
 
-# Replace this with your code
+#             (...etc.)
+
+while True:
+    choice = input("Enter your equation > ")
+    token = choice.split(' ')
+    if token[0] == "q":
+        print("We will exit")
+        break
+
+    elif token[0] == "+":
+        result = add(token[1],token[2])
+        
+    elif token[0] == "-":
+        result =  subtract(token[1], token[2])
+
+    elif token[0] == "*":
+        result = multiply(token[1],token[2])
+   
+    elif token[0] == "/":
+        result = divide(token[1], token[2])
+    
+    elif token[0] == "square":
+        result = square(token[1])
+
+    elif token[0] == "cube":
+        result = cube(token[1])
+
+    elif token[0] == "power":
+        result = power(token[1], token[2])
+
+    elif token[0] == "mod":
+        result = mod(token[1],token[2])
+#
+    else:
+        print(result)
